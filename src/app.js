d0be7785f1f075ead745b73b8f07662f05368ca5
SaulyS01 <q931534326@gmail.com>
2022-05-24 22:52:28 -0500

    v1 proyecto base en heroku

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import express from 'express'
 import morgan from 'morgan'
 
+import postRoutes from './routes/post.routes'
+
 const app = express();
 var cors = require('cors');
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
-
+app.use('/api/auth/post', postRoutes);
 app.get('/', (req, res) => {
     res.send('Bienvenido a Node JS');
 });
+
+
 
 export default app;
