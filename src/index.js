a9af847694f7ac07ac2eebe7be7d512292bcf538
SaulyS01 <q931534326@gmail.com>
2022-05-24 19:58:31 -0500

    v1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import app from './app'
 
-app.listen(process.env.PORT || 3000);
-console.log('server listen on port: ', 3000);
+server.listen(port,()=>{  // do not add localhost here if you are deploying it
+    console.log("server listening to port "+port);
+});
